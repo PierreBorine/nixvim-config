a35be1698516618968a0e141f28e6b77bb9416e9
PierreBorine <pierre.borine@protonmail.com>
2025-03-30 04:09:33 +0200

    removed fugitive & trouble

--- config/plugins/tools.nix
+++ config/plugins/tools.nix
@@ -12,15 +12,6 @@
       lazyLoad.settings.cmd = "Yazi";
     };
     lazygit.enable = true;
-    fugitive.enable = true;
-    trouble = {
-      enable = true;
-      lazyLoad.settings.cmd = "Trouble";
-      settings = {
-        auto_jump = true;
-        open_no_results = true;
-      };
-    };
 
     colorizer = {
       enable = true;
@@ -61,8 +52,6 @@
         use_default_keymaps = false;
       };
     };
-
-    noice.settings.presets.inc_rename = true;
   };
 
   keymaps = [
@@ -98,37 +87,6 @@
       key = "<leader>gg";
       action = "<cmd>LazyGit<CR>";
       options.desc = "Open LazyGit";
-    }
-
-    {
-      key = "<leader>tx";
-      action = "<cmd>Trouble diagnostics toggle<CR>";
-      options.desc = "Diagnostics (Trouble)";
-    }
-    {
-      key = "<leader>tX";
-      action = "<cmd>Trouble diagnostics toggle filter.buf=0<CR>";
-      options.desc = "Buffer Diagnostics (Trouble)";
-    }
-    {
-      key = "<leader>cs";
-      action = "<cmd>Trouble symbols toggle focus=false<CR>";
-      options.desc = "Symbols (Trouble)";
-    }
-    {
-      key = "<leader>cl";
-      action = "<cmd>Trouble lsp toggle focus=false win.position=right<CR>";
-      options.desc = "LSP Definitions / references / ... (Trouble)";
-    }
-    {
-      key = "<leader>tL";
-      action = "<cmd>Trouble loclist toggle<CR>";
-      options.desc = "Location List (Trouble)";
-    }
-    {
-      key = "<leader>tQ";
-      action = "<cmd>Trouble qflist toggle<CR>";
-      options.desc = "Quickfix List (Trouble)";
     }
   ];
 }
