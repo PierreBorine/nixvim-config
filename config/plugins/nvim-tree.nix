93e491db3008f359af29888dd5340a3e4373c635
PierreBorine <pierre.borine@protonmail.com>
2025-08-19 18:42:22 +0200

    fix: follow nixvim changes

--- config/plugins/nvim-tree.nix
+++ config/plugins/nvim-tree.nix
@@ -1,29 +1,31 @@
 {
   plugins.nvim-tree = {
     enable = true;
-    disableNetrw = true;
-    hijackCursor = true;
-    syncRootWithCwd = true;
-    updateFocusedFile = {
-      enable = true;
-      updateRoot = false;
-    };
-    view.preserveWindowProportions = true;
-    renderer = {
-      rootFolderLabel = false;
-      highlightGit = true;
-      indentMarkers.enable = true;
-      icons = {
-        glyphs = {
-          default = "󰈚";
-          folder = {
-            default = "";
-            empty = "";
-            emptyOpen = "";
-            open = "";
-            symlink = "";
+    settings = {
+      disable_netrw = true;
+      hijack_cursor = true;
+      sync_root_with_cwd = true;
+      update_focused_file = {
+        enable = true;
+        update_root = true;
+      };
+      view.preserve_window_proportions = true;
+      renderer = {
+        root_folder_label = false;
+        highlight_git = true;
+        indent_markers.enable = true;
+        icons = {
+          glyphs = {
+            default = "󰈚";
+            folder = {
+              default = "";
+              empty = "";
+              empty_open = "";
+              open = "";
+              symlink = "";
+            };
+            git.unmerged = "";
           };
-          git.unmerged = "";
         };
       };
     };
