4ab2e2799f75be4fdf43432f22e5c25af73e5666
PierreBorine <pierre.borine@protonmail.com>
2025-08-20 21:28:16 +0200

    refactor: strip nvim from pkgs filenames

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,8 +1,8 @@
-prev: final: inputs: {
+prev: _: inputs: {
   extraPlugins = {
-    live-server = prev.callPackage ./live-server-nvim.nix {inherit inputs;};
-    lualine-so-fancy = prev.callPackage ./lualine-so-fancy-nvim.nix {inherit inputs;};
-    nvim-toggler = prev.callPackage ./nvim-toggler.nix {inherit inputs;};
-    sudoku = prev.callPackage ./sudoku-nvim.nix {inherit inputs;};
+    live-server = prev.callPackage ./live-server.nix {inherit inputs;};
+    lualine-so-fancy = prev.callPackage ./lualine-so-fancy.nix {inherit inputs;};
+    nvim-toggler = prev.callPackage ./toggler.nix {inherit inputs;};
+    sudoku = prev.callPackage ./sudoku.nix {inherit inputs;};
   };
 }
