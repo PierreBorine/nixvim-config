882490b7ef5f340f412537bc3c9d2e48720cdf5a
PierreBorine <pierre.borine@protonmail.com>
2025-10-08 14:12:40 +0200

    fix(ccls): build issues with new cmake version

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -19,7 +19,18 @@
         };
       };
       bashls.enable = true;
-      ccls.enable = true; # C / C++
+      ccls = {
+        enable = true; # C / C++
+        # https://github.com/NixOS/nixpkgs/issues/449588
+        package = pkgs.ccls.overrideAttrs {
+          src = pkgs.fetchFromGitHub {
+            owner = "MaskRay";
+            repo = "ccls";
+            rev = "5660367c771345b68c4ead4a4db2d4786985bf78";
+            hash = "sha256-R+5pL0orUdHtquqvJa4esNmc6ETbX8WK5oJlBCSG+uI=";
+          };
+        };
+      };
       csharp_ls.enable = true; # C#
       ts_ls.enable = true; # Javascript / Typescript
       # Haskell
