41d86d413bb89650fc0bbec77aa9ccd11404e4c3
PierreBorine <pierre.borine@protonmail.com>
2025-11-08 13:10:34 +0100

    feat: better up/down
    From LazyVim

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -6,6 +6,46 @@
       action = ":";
     }
     {
+      mode = ["n" "x"];
+      key = "j";
+      action = "v:count == 0 ? 'gj' : 'j'";
+      options = {
+        desc = "Down";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      mode = ["n" "x"];
+      key = "<Down>";
+      action = "v:count == 0 ? 'gj' : 'j'";
+      options = {
+        desc = "Down";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      mode = ["n" "x"];
+      key = "k";
+      action = "v:count == 0 ? 'gk' : 'k'";
+      options = {
+        desc = "Up";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
+      mode = ["n" "x"];
+      key = "<Up>";
+      action = "v:count == 0 ? 'gk' : 'k'";
+      options = {
+        desc = "Up";
+        expr = true;
+        silent = true;
+      };
+    }
+    {
       key = "<C-s>";
       action = "<cmd>w<CR>";
       options.desc = "Save file";
