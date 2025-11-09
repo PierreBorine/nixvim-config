8d484e5d6ce8cd400af80ff0ce5fe5afad8dabb3
PierreBorine <pierre.borine@protonmail.com>
2025-11-09 04:33:10 +0100

    feat(nvim-tree): better navigation & don't update root

--- config/plugins/nvim-tree.nix
+++ config/plugins/nvim-tree.nix
@@ -7,19 +7,12 @@
         "NvimTreeFocus"
         "NvimTreeOpen"
       ];
-      keys = [
-        "<C-n>"
-        "<leader>e"
-      ];
+      keys = ["<leader>e"];
     };
     settings = {
       disable_netrw = true;
       hijack_cursor = true;
       sync_root_with_cwd = true;
-      update_focused_file = {
-        enable = true;
-        update_root = true;
-      };
       view.preserve_window_proportions = true;
       renderer = {
         root_folder_label = false;
@@ -39,7 +32,28 @@
           };
         };
       };
+      on_attach.__raw = ''
+        function(bufnr)
+          local api = require("nvim-tree.api")
+          local function opts(desc)
+            return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+          end
+          -- default mappings
+          api.config.mappings.default_on_attach(bufnr)
+
+          -- custom mappings
+          vim.keymap.set("n", "?",        api.tree.toggle_help, opts("Help"))
+          vim.keymap.set("n", "l",        api.node.open.edit,   opts("Open"))
+          vim.keymap.set("n", "<Right>",  api.node.open.edit,   opts("Open"))
+        end
+      '';
     };
+  };
+
+  # Make sure netrw is disabled
+  globals = {
+    loaded_netrw = 1;
+    loaded_netrwPlugin = 1;
   };
 
   keymaps = [
