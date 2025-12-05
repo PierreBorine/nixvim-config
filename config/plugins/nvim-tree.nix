55f325cc33c7cbb9b479599b4e8aa2ae4fd14cea
PierreBorine <pierre.borine@protonmail.com>
2025-12-05 23:34:47 +0100

    feat(nvim-tree): left keys to close

--- config/plugins/nvim-tree.nix
+++ config/plugins/nvim-tree.nix
@@ -42,9 +42,11 @@
           api.config.mappings.default_on_attach(bufnr)
 
           -- custom mappings
-          vim.keymap.set("n", "?",        api.tree.toggle_help, opts("Help"))
-          vim.keymap.set("n", "l",        api.node.open.edit,   opts("Open"))
-          vim.keymap.set("n", "<Right>",  api.node.open.edit,   opts("Open"))
+          vim.keymap.set("n", "?",        api.tree.toggle_help,           opts("Help"))
+          vim.keymap.set("n", "h",        api.node.navigate.parent_close, opts("Close Directory"))
+          vim.keymap.set("n", "l",        api.node.open.edit,             opts("Open"))
+          vim.keymap.set("n", "<Left>",   api.node.navigate.parent_close, opts("Close Directory"))
+          vim.keymap.set("n", "<Right>",  api.node.open.edit,             opts("Open"))
         end
       '';
     };
