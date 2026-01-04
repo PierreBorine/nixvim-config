f7429bbf5909015f2464e7617ed69c91ef620f64
PierreBorine <pierre.borine@protonmail.com>
2026-01-04 02:55:16 +0100

    fix: remove old lualine hack

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,25 +1,4 @@
 prev: inputs: {
   c-formatter-42 = prev.python3Packages.callPackage ./c-formatter-42.nix {};
   extraPlugins = prev.callPackage ./plugins.nix {inherit inputs;};
-
-  # TEMP: nixpkgs issue, remove next update
-  vimPlugins =
-    prev.vimPlugins
-    // {
-      lualine-nvim = prev.neovimUtils.buildNeovimPlugin {
-        luaAttr = prev.luaPackages.lualine-nvim.overrideAttrs {
-          knownRockspec =
-            (prev.fetchurl {
-              url = "mirror://luarocks/lualine.nvim-scm-1.rockspec";
-              sha256 = "01cqa4nvpq0z4230szwbcwqb0kd8cz2dycrd764r0z5c6vivgfzs";
-            }).outPath;
-          src = prev.fetchFromGitHub {
-            owner = "nvim-lualine";
-            repo = "lualine.nvim";
-            rev = "47f91c416daef12db467145e16bed5bbfe00add8";
-            hash = "sha256-OpLZH+sL5cj2rcP5/T+jDOnuxd1QWLHCt2RzloffZOA=";
-          };
-        };
-      };
-    };
 }
